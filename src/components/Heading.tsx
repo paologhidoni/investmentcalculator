90ff83540b4e949b51e76a5ec773716c915c74ca
paologhidoni <paolo.ghidoni82@gmail.com>
2025-03-01 14:09:13 +0100

    Update h2 font size and border rounding

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const Heading: React.FC<Props> = ({ children }) => {
   return (
-    <h2 className="uppercase tracking-wide bg-gradient-to-r from-black to-purple-600 bg-clip-text text-transparent text-4xl text-center p-2 border-5 border-b-0 border-(--secondary-color) rounded-t-full">
+    <h2 className="uppercase tracking-wide bg-gradient-to-r from-black to-purple-600 bg-clip-text text-transparent text-xl md:text-3xl text-center p-2 border-5 border-b-0 border-(--secondary-color) rounded-t-lg">
       {children}
     </h2>
   );
